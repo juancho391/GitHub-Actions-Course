251cc903e8621b7b2ef40ab283e9f414d3680426
juancho391 <juanchischis2010@hotmail.com>
2025-07-22 10:46:09 -0500

    uso de variables de entorno con variables de entorno que vienne de el workflow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 
 username = os.getenv("USERNAME")
+language = os.getenv("LENGUAJE")
 
-print(f"Hello, {username}! Welcome to the GitHub Actions Course.")
+print(
+    f"Hello, {username}! Welcome to the GitHub Actions Course, your favorite language is {language}."
+)
